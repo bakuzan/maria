0f9f20eccfe84e95fe56beef05ed1e8fcb99e978
bakuzan <imannoyed4days@hotmail.co.uk>
2019-07-25 10:02:25 +0100

    added keyboard shortcuts..2/3 work.

--- src/background.js
+++ src/background.js
@@ -3,11 +3,28 @@ import './thirdParty/chrome-extension-async.js';
 import {
   PROCESS_NUMBERS,
   REMOVE_LINKS,
-  FETCH_NUMBER_DETAIL
+  FETCH_NUMBER_DETAIL,
+  BASE_LINK_URL
 } from './consts.js';
 import fetch from './utils/fetch.js';
 import injectContentModule from './utils/injectContentModule.js';
 import userFeedback from './utils/userFeedback.js';
+import getActiveTab from './utils/getActiveTab.js';
+
+/* Common */
+function processLinks(tabId, sendResponse = () => null) {
+  injectContentModule(tabId, `addLinks.js`);
+  sendResponse({ action: PROCESS_NUMBERS, message: 'Done' });
+}
+
+function removeLinks(tabId, sendResponse = () => null) {
+  injectContentModule(tabId, `removeLinks.js`);
+  sendResponse({ action: REMOVE_LINKS, message: 'Done' });
+}
+
+/* Common END */
+
+/* Message handling */
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log(
@@ -18,13 +35,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
   switch (request.action) {
     case PROCESS_NUMBERS:
-      injectContentModule(request.tabID, `addLinks.js`);
-      sendResponse({ action: PROCESS_NUMBERS, message: 'Done' });
+      processLinks(request.tabID, sendResponse);
       break;
 
     case REMOVE_LINKS:
-      injectContentModule(request.tabID, `removeLinks.js`);
-      sendResponse({ action: REMOVE_LINKS, message: 'Done' });
+      removeLinks(request.tabID, sendResponse);
       break;
 
     case FETCH_NUMBER_DETAIL:
@@ -65,20 +80,60 @@ async function openWindow(selectionText) {
   }
 }
 
+function onSelectedText(selectionText = '', showAlert = true) {
+  const selectionIsValid = /^\d+$/.test(selectionText);
+
+  if (selectionIsValid) {
+    openWindow(selectionText);
+  } else if (showAlert) {
+    userFeedback(
+      'warning',
+      'The current selection is not a valid magic number'
+    );
+  }
+}
+
 chrome.contextMenus.create({
   title: 'View magic number "%s"',
   contexts: ['selection'],
   onclick: function(info) {
     const { selectionText } = info;
-    const selectionIsValid = /^\d+$/.test(selectionText);
-
-    if (selectionIsValid) {
-      openWindow(selectionText);
-    } else {
-      userFeedback(
-        'warning',
-        'The current selection is not a valid magic number'
-      );
-    }
+    onSelectedText(selectionText);
   }
+});
+
+/* Keyboard shortcuts */
+const CMD_ADD_LINKS = 'add-links';
+const CMD_REMOVE_LINKS = 'remove-links';
+const CMD_VIEW_MAGIC_NUMBER = 'view-magic-number';
+
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('Command:', command);
+  chrome.commands.getAll(function(cmds) {
+    console.log(cmds);
+  });
+
+  getActiveTab().then((activeTab) => {
+    switch (command) {
+      case CMD_ADD_LINKS:
+        processLinks(activeTab.id);
+        break;
+      case CMD_REMOVE_LINKS:
+        removeLinks(activeTab.id);
+        break;
+      case CMD_VIEW_MAGIC_NUMBER:
+        chrome.tabs.executeScript(
+          activeTab.id,
+          {
+            code: `window.getSelection().toString();`
+          },
+          function(text) {
+            onSelectedText(text, false);
+          }
+        );
+        break;
+      default:
+        return;
+    }
+  });
 });
