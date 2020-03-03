0b31deedafd8d368eac9742af789a23ef065fa32
bakuzan <imannoyed4days@hotmail.co.uk>
2020-03-03 15:47:47 +0000

    download gallery feature.

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,9 +1,12 @@
 import '../styles.scss';
 import './popup.scss';
 import { browser } from 'webextension-polyfill-ts';
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
 
 import { MariaAction } from '@/consts';
 import getActiveTab from '@/utils/getActiveTab';
+import downloadDriver from '@/utils/downloadDriver';
 
 function buttonListener(action: MariaAction) {
   return async function() {
@@ -21,10 +24,65 @@ function buttonListener(action: MariaAction) {
   };
 }
 
-document
-  .getElementById('processNumbers')
-  .addEventListener('click', buttonListener(MariaAction.PROCESS_NUMBERS));
+async function downloadGallery() {
+  const images = document.querySelectorAll<HTMLImageElement>(''); // TODO selector?
+  downloadDriver.init(images.length);
 
-document
-  .getElementById('removeLinks')
-  .addEventListener('click', buttonListener(MariaAction.REMOVE_LINKS));
+  const zip = new JSZip();
+
+  const items = Array.from(images).map((x, i) => {
+    const rawUrl = x.getAttribute('data-src-set'); // TODO check attribute name
+    const ext = rawUrl.split('.').pop();
+    const name = i.toString().padStart(3, '0');
+
+    return {
+      name: `${name}.${ext}`,
+      url: rawUrl.replace('//t.', '//i.').replace(`t.${ext}`, `.${ext}`)
+    };
+  });
+
+  for (const item of items) {
+    downloadDriver.bumpLoadingCount();
+
+    const response = await fetch(item.url);
+    const img = await response.arrayBuffer();
+
+    zip.file(item.name, img);
+    downloadDriver.bumpLoadedCount();
+  }
+
+  downloadDriver.zipping();
+  zip.generateAsync({ type: 'blob' }).then(function(content) {
+    // TODO
+    // Get name from page title?
+    FileSaver.saveAs(content, 'maria_gallery_download.zip');
+    downloadDriver.reset();
+  });
+}
+
+async function run() {
+  document
+    .getElementById('processNumbers')
+    .addEventListener('click', buttonListener(MariaAction.PROCESS_NUMBERS));
+
+  document
+    .getElementById('removeLinks')
+    .addEventListener('click', buttonListener(MariaAction.REMOVE_LINKS));
+
+  const activeTab = await getActiveTab();
+  const re = /nhentai.net\/g\/\d{1,}$/;
+  const isGalleryPage = new RegExp(re).test(activeTab.url);
+
+  if (isGalleryPage) {
+    document.getElementById('downloadGalleryOption').style.display = 'block';
+
+    const downloadButton = document.querySelector<HTMLButtonElement>(
+      '#downloadGallery'
+    );
+
+    downloadButton.disabled = false;
+    downloadButton.addEventListener('click', downloadGallery);
+  }
+}
+
+run();
